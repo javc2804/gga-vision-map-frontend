b2069f6bc08b288f5fb10eeffa2a3b31ec082d5a
javier <javc2804@gmail.com>
2024-04-27 18:29:19 -0400

    alert

--- src/auth/pages/register.tsx
+++ src/auth/pages/register.tsx
@@ -1,3 +1,6 @@
+import { Alert } from "@mui/material";
+import { Snackbar } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 import {
@@ -10,10 +13,30 @@ import {
 } from "./styles";
 
 export const Register = () => {
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    severity: "",
+  });
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    // Aquí va tu lógica de registro
+    // Si el registro es exitoso
+    setAlert({
+      show: true,
+      message: "Registrado con éxito",
+      severity: "success",
+    });
+    setTimeout(() => setAlert({ ...alert, show: false }), 4000);
+    // Si el registro falla
+    // setAlert({ show: true, message: 'Error al registrar', severity: 'error' });
+  };
+
   return (
     <>
       <AuthLayout title={"Registrar usuario"}>
-        <form className="">
+        <form className="" onSubmit={handleRegister}>
           <GridContainer container>
             <InputContainer item xs={12}>
               <StyledTextField
@@ -37,6 +60,16 @@ export const Register = () => {
                 name="password"
               />
             </InputContainer>
+            <Snackbar
+              open={alert.show}
+              autoHideDuration={4000}
+              onClose={() => setAlert({ ...alert, show: false })}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            >
+              <Alert variant="filled" severity={alert.severity}>
+                {alert.message}
+              </Alert>
+            </Snackbar>
             <GridContainer container spacing={2}>
               <GridItem item xs={12} sm={6}>
                 <StyledButton type="submit" variant="contained" fullWidth>
