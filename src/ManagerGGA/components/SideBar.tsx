6b32a1ff07dca8eca68f12feaea728d356eaa7a5
javier <javc2804@gmail.com>
2024-05-01 13:03:30 -0400

    menu

--- src/ManagerGGA/components/SideBar.tsx
+++ src/ManagerGGA/components/SideBar.tsx
@@ -1,9 +1,21 @@
-import { TurnedInNot } from "@mui/icons-material";
+import {
+  ExpandLess,
+  ExpandMore,
+  MoneyOffCsred,
+  AddBox,
+  List as ListIcon,
+  Store,
+  Note,
+  Inventory,
+  People,
+  PersonAdd,
+  PeopleOutline,
+} from "@mui/icons-material";
 import {
   Box,
+  Collapse,
   Divider,
   Drawer,
-  Grid,
   List,
   ListItem,
   ListItemButton,
@@ -13,12 +25,40 @@ import {
   Typography,
 } from "@mui/material";
 import Logo from "../../assets/logo.png";
+import { useState } from "react";
 
 export const SideBar = ({ drawerWidth = 240, open, onClose }) => {
   let menu = JSON.parse(localStorage.getItem("menu") || "[]");
   if (!Array.isArray(menu)) {
     menu = [menu];
   }
+
+  const [openSubMenu, setOpenSubMenu] = useState({});
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleClick = (name) => {
+    setOpenSubMenu((prevOpenSubMenu) => ({
+      ...prevOpenSubMenu,
+      [name]: !prevOpenSubMenu[name],
+    }));
+    setSelectedItem((prevSelectedItem) =>
+      prevSelectedItem === name ? null : name
+    );
+  };
+
+  const iconMap = {
+    Money: <MoneyOffCsred />,
+    AddBox: <AddBox />,
+    List: <ListIcon />,
+    Store: <Store />,
+    Note: <Note />,
+    Inventory: <Inventory />,
+    People: <People />,
+    PersonAdd: <PersonAdd />,
+    PeopleOutline: <PeopleOutline />,
+    // Add more icons as needed
+  };
+
   return (
     <Box
       component="nav"
@@ -34,44 +74,83 @@ export const SideBar = ({ drawerWidth = 240, open, onClose }) => {
         }}
       >
         <Toolbar sx={{ justifyContent: "center", flexDirection: "column" }}>
-          {" "}
           <img
             src={Logo}
             alt="Logo"
             style={{
               width: "200px",
               height: "auto",
-              marginTop: "10px",
               marginBottom: "10px",
+              marginTop: "20px",
             }}
           />
           <Typography variant="h6" noWrap>
-            GGA - Administración
+            Adminitración
           </Typography>
         </Toolbar>
         <Divider />
         <List>
           {menu.map((item) => (
             <div key={item.name}>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <ListItemText primary={item.name} />
-                </ListItemButton>
-              </ListItem>
-              {item.subMenu &&
-                item.subMenu.map((subItem) => (
-                  <ListItem key={subItem} disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        <TurnedInNot />
-                      </ListItemIcon>
-                      <ListItemText primary={subItem} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
+              <ListItemButton
+                onClick={() => handleClick(item.name)}
+                selected={selectedItem === item.name}
+                style={{
+                  backgroundColor:
+                    selectedItem === item.name ? "#f5447a" : "inherit",
+                  color: selectedItem === item.name ? "#ffffff" : "inherit",
+                }}
+              >
+                <ListItemIcon
+                  style={{
+                    color: selectedItem === item.name ? "#ffffff" : "inherit",
+                  }}
+                >
+                  {iconMap[item.icon]}
+                </ListItemIcon>
+                <ListItemText primary={item.name} />
+                {openSubMenu[item.name] ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse
+                in={openSubMenu[item.name]}
+                timeout="auto"
+                unmountOnExit
+              >
+                <List component="div" disablePadding>
+                  {item.subMenu &&
+                    item.subMenu.map((subItem) => (
+                      <ListItem key={subItem.name} disablePadding>
+                        <ListItemButton
+                          onClick={() => setSelectedItem(subItem.name)}
+                          selected={selectedItem === subItem.name}
+                          style={{
+                            backgroundColor:
+                              selectedItem === subItem.name
+                                ? "#f5447a"
+                                : "inherit",
+                            color:
+                              selectedItem === subItem.name
+                                ? "#ffffff"
+                                : "inherit",
+                            marginLeft: "8px",
+                          }}
+                        >
+                          <ListItemIcon
+                            style={{
+                              color:
+                                selectedItem === subItem.name
+                                  ? "#ffffff"
+                                  : "inherit",
+                            }}
+                          >
+                            {iconMap[subItem.icon]}
+                          </ListItemIcon>
+                          <ListItemText primary={subItem.name} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                </List>
+              </Collapse>
             </div>
           ))}
         </List>
