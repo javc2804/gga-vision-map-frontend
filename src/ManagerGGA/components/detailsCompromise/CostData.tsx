d76ef260cb1a960842339c964ca6a66a8b7cb989
javier <javc2804@gmail.com>
2024-05-22 18:28:47 -0400

    costo

--- src/ManagerGGA/components/detailsCompromise/CostData.tsx
+++ src/ManagerGGA/components/detailsCompromise/CostData.tsx
@@ -23,139 +23,40 @@ export const CostData = ({ invoiceData, onValuesChange, style }) => {
   };
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Box>Costos/Otros datos</Box>
-      <Grid container spacing={5}>
-        <Grid item xs={4}>
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
           <TextField
-            label="Precio Unitario Divisas"
+            label="Precio Unitario Bs"
             value={invoiceData.precioUnitarioDivisas || ""}
             onChange={handleInputChange("precioUnitarioDivisas")}
             fullWidth
             style={{ marginBottom: "20px" }}
           />
+        </Grid>
+        <Grid item xs={3}>
           <TextField
-            label="Monto Total Pago Bolivares"
+            label="Monto Total Bs"
             value={invoiceData.montoTotalPagoBolivares || ""}
             onChange={handleInputChange("montoTotalPagoBolivares")}
             fullWidth
             style={{ marginBottom: "20px" }}
           />
-          <TextField
-            label="Monto Total Divisas Deuda"
-            value={invoiceData.montoTotalDivisasDeuda || ""}
-            onChange={handleInputChange("montoTotalDivisasDeuda")}
-            fullWidth
-            style={{ marginBottom: "20px" }}
-          />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
           <TextField
-            label="Precio Unitario Divisas S"
+            label="Precio Unitario $"
             value={invoiceData.precioUnitarioDivisasS || ""}
             onChange={handleInputChange("precioUnitarioDivisasS")}
             fullWidth
             style={{ marginBottom: "20px" }}
           />
-          <TextField
-            label="Monto Total Pago Divisas"
-            value={invoiceData.montoTotalPagoDivisas || ""}
-            onChange={handleInputChange("montoTotalPagoDivisas")}
-            fullWidth
-            style={{ marginBottom: "20px" }}
-          />
-          <DatePicker
-            label="Fecha Entrega"
-            value={invoiceData.fechaEntrega || null}
-            onChange={handleDateChange("fechaEntrega")}
-            components={{
-              textField: ({ inputRef, inputProps, InputProps }) => (
-                <TextField
-                  {...inputProps}
-                  InputProps={InputProps}
-                  ref={inputRef}
-                  fullWidth
-                />
-              ),
-            }}
-            style={{ marginBottom: "20px" }}
-          />
         </Grid>
-        <Grid item xs={4}>
-          <Box marginBottom={2}>
-            <DatePicker
-              label="Fecha Pago"
-              value={invoiceData.fechaPago || null}
-              onChange={handleDateChange("fechaPago")}
-              components={{
-                textField: ({ inputRef, inputProps, InputProps }) => (
-                  <TextField
-                    {...inputProps}
-                    InputProps={InputProps}
-                    ref={inputRef}
-                    fullWidth
-                  />
-                ),
-              }}
-              style={{ marginBottom: "20px" }}
-            />
-          </Box>
+        <Grid item xs={3}>
           <TextField
-            label="Orden Pago Numero"
-            value={invoiceData.ordenPagoNumero || ""}
-            onChange={handleInputChange("ordenPagoNumero")}
+            label="Monto Total $"
+            value={invoiceData.montoTotalDivisasDeuda || ""}
+            onChange={handleInputChange("montoTotalDivisasDeuda")}
             fullWidth
-            style={{ marginBottom: "20px" }}
-          />
-          <TextField
-            label="Orden Compra Servicio"
-            value={invoiceData.ordenCompraServicio || ""}
-            onChange={handleInputChange("ordenCompraServicio")}
-            fullWidth
-            style={{ marginBottom: "20px" }}
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <Box marginBottom={2}>
-            <DatePicker
-              label="Orden Compra Servicio Fecha"
-              value={invoiceData.ordenCompraServicioFecha || null}
-              onChange={handleDateChange("ordenCompraServicioFecha")}
-              components={{
-                textField: ({ inputRef, inputProps, InputProps }) => (
-                  <TextField
-                    {...inputProps}
-                    InputProps={InputProps}
-                    ref={inputRef}
-                    fullWidth
-                  />
-                ),
-              }}
-              style={{ marginBottom: "20px" }}
-            />
-          </Box>
-          <TextField
-            label="Nota Entrega Numero"
-            value={invoiceData.notaEntregaNumero || ""}
-            onChange={handleInputChange("notaEntregaNumero")}
-            fullWidth
-            style={{ marginBottom: "20px" }}
-          />
-          <TextField
-            label="Estatus"
-            value={invoiceData.estatus || ""}
-            onChange={handleInputChange("estatus")}
-            fullWidth
-            style={{ marginBottom: "20px" }}
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <TextField
-            label="Observación"
-            value={invoiceData.observacion || ""}
-            onChange={handleInputChange("observacion")}
-            fullWidth
-            multiline
-            rows={4}
             style={{ marginBottom: "20px" }}
           />
         </Grid>
