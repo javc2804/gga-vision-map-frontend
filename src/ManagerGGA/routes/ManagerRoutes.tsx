781239827c3a82dca98b244d0b9f82843e869ef0
javier <javc2804@gmail.com>
2024-05-22 18:19:10 -0400

    rous

--- src/ManagerGGA/routes/ManagerRoutes.tsx
+++ src/ManagerGGA/routes/ManagerRoutes.tsx
@@ -2,14 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { ManagerGGAPage } from "../pages/ManagerGGAPage";
 import ListPurchasePage from "../pages/ListPurchasePage";
 import RegisterOutPage from "../pages/RegisterOutPage";
-import DetailPurchasePage from "../pages/DetailPurchasePage";
+import DetailCompromisePage from "../pages/DetailCompromisePage";
 
 const ManagerRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<ManagerGGAPage />} />
       <Route path="/register-out/:id?" element={<RegisterOutPage />} />
-      <Route path="/notes-store" element={<DetailPurchasePage />} />
+      <Route path="/notes-store" element={<DetailCompromisePage />} />
       <Route path="/list-purchases" element={<ListPurchasePage />} />
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
