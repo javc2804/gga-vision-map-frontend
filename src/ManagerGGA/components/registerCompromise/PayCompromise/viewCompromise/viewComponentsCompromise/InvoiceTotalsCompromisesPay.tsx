102b7ad18c06d6bbe50df57d8a63b7aa2c592f0e
javier <javc2804@gmail.com>
2024-06-01 13:08:08 -0400

    ajuste en pago de compromiso de botones

--- src/ManagerGGA/components/registerCompromise/PayCompromise/viewCompromise/viewComponentsCompromise/InvoiceTotalsCompromisesPay.tsx
+++ src/ManagerGGA/components/registerCompromise/PayCompromise/viewCompromise/viewComponentsCompromise/InvoiceTotalsCompromisesPay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, TextField, Button, Grid } from "@mui/material";
 
 interface InvoiceTotalsProps {
   totalFactUsd: number;
@@ -9,7 +9,7 @@ interface InvoiceTotalsProps {
   setTotalFactBs: (value: number) => void;
   handleSaveClick: () => void;
 }
-export const InvoiceTotalsComrpomisesPay: React.FC<InvoiceTotalsProps> = ({
+export const InvoiceTotalsCompromisesPay: React.FC<InvoiceTotalsProps> = ({
   totalFactUsd,
   totalFactBs,
   compromise,
@@ -21,30 +21,59 @@ export const InvoiceTotalsComrpomisesPay: React.FC<InvoiceTotalsProps> = ({
     compromise && compromise.response ? compromise.response.montoTotalUsd : 0;
 
   return (
-    <>
-      <TextField
-        label="Total factura $"
-        variant="outlined"
-        value={totalFactUsd.toString()}
-        onChange={(e) => setTotalFactUsd(Number(e.target.value))}
-      />
-      <TextField
-        label="Total deuda $ "
-        variant="outlined"
-        value={(totalDeuda - totalFactUsd).toString()}
-        onChange={(e) => setTotalFactBs(Number(e.target.value))}
-      />
-      <Button variant="contained" color="primary" onClick={handleSaveClick}>
-        Guardar
-      </Button>
-      <TextField
-        label="Total factura Bs"
-        variant="outlined"
-        value={totalFactBs.toString()}
-        onChange={(e) => setTotalFactBs(Number(e.target.value))}
-      />
-    </>
+    <Grid
+      container
+      direction="row"
+      justifyContent="flex-end"
+      alignItems="flex-end"
+      spacing={2}
+    >
+      <Grid item>
+        <Grid container direction="column">
+          <Grid item>
+            <TextField
+              label="Total factura $"
+              variant="outlined"
+              value={totalFactUsd.toString()}
+              onChange={(e) => setTotalFactUsd(Number(e.target.value))}
+            />
+          </Grid>
+          <Grid item style={{ marginTop: "20px" }}>
+            <TextField
+              label="Total deuda $ "
+              variant="outlined"
+              value={(totalDeuda - totalFactUsd).toString()}
+              onChange={(e) => setTotalFactBs(Number(e.target.value))}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction="column">
+          <Grid item>
+            <TextField
+              label="Total factura Bs"
+              variant="outlined"
+              value={totalFactBs.toString()}
+              onChange={(e) => setTotalFactBs(Number(e.target.value))}
+            />
+          </Grid>
+          <Grid item style={{ marginTop: "20px" }}>
+            <TextField
+              label="Cantidad Deuda"
+              variant="outlined"
+              // Asegúrate de agregar el estado y el manejador de eventos para este campo
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} container justifyContent="flex-end">
+        <Button variant="contained" color="primary" onClick={handleSaveClick}>
+          Guardar
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
-export default InvoiceTotalsComrpomisesPay;
+export default InvoiceTotalsCompromisesPay;
