09971bb0c2aab7b010f88cf8a1f34f1d76e66667
javier <javc2804@gmail.com>
2024-06-11 18:21:42 -0400

    maqueta de dialog eliminar

--- src/ManagerGGA/components/ListPurchase/TableRowData.tsx
+++ src/ManagerGGA/components/ListPurchase/TableRowData.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TableRow, TableCell, Box } from "@mui/material";
 import { IRow } from "../../view/expenses/ListPurchase";
 import EditIcon from "@mui/icons-material/Edit";
@@ -5,9 +6,11 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { editPurchase } from "../../../store/purchase/purchaseSlice";
+import { DeleteDialog } from "../../../components/DeleteDialog"; // Asegúrate de importar el componente DeleteDialog
 
 type ActionButtonsProps = {
   id: any;
+  handleDelete: () => void;
 };
 
 type TableRowDataProps = {
@@ -23,7 +26,10 @@ const cursor = {
   cursor: "pointer",
 };
 
-export const ActionButtons: React.FC<ActionButtonsProps> = ({ id }) => {
+export const ActionButtons: React.FC<ActionButtonsProps> = ({
+  id,
+  handleDelete,
+}) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleEdit = (data: any) => {
@@ -31,39 +37,53 @@ export const ActionButtons: React.FC<ActionButtonsProps> = ({ id }) => {
     navigate(`/register-out/`);
   };
 
-  const handleDelete = (data: any) => {
-    console.log(data);
-  };
-
   return (
     <Box>
       <EditIcon style={cursor} color="primary" onClick={() => handleEdit(id)} />
-      <DeleteIcon
-        style={cursor}
-        color="error"
-        onClick={() => handleDelete(id)}
-      />
+      <DeleteIcon style={cursor} color="error" onClick={handleDelete} />
     </Box>
   );
 };
 
 export const TableRowData: React.FC<TableRowDataProps> = ({ row, headers }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = () => {
+    console.log(row.ID);
+    setOpen(false);
+  };
+
   return (
-    <TableRow key={row.ID}>
-      {headers.map((header, index) => {
-        const dataKey = columnToDataKeyMap[header] || header;
-        return dataKey !== "acciones" ? (
-          <TableCell key={`${row.ID}-${index}`}>
-            {dataKey === "createdAt"
-              ? new Date(row[dataKey]).toLocaleDateString("en-GB") // Formatear la fecha si la clave de los datos es 'createdAt'
-              : row[dataKey]}
-          </TableCell>
-        ) : (
-          <TableCell key={`${row.ID}-actions`}>
-            <ActionButtons id={row} />
-          </TableCell>
-        );
-      })}
-    </TableRow>
+    <>
+      <TableRow key={row.ID}>
+        {headers.map((header, index) => {
+          const dataKey = columnToDataKeyMap[header] || header;
+          return dataKey !== "acciones" ? (
+            <TableCell key={`${row.ID}-${index}`}>
+              {dataKey === "createdAt"
+                ? new Date(row[dataKey]).toLocaleDateString("en-GB") // Formatear la fecha si la clave de los datos es 'createdAt'
+                : row[dataKey]}
+            </TableCell>
+          ) : (
+            <TableCell key={`${row.ID}-actions`}>
+              <ActionButtons id={row} handleDelete={handleDelete} />
+            </TableCell>
+          );
+        })}
+      </TableRow>
+      <DeleteDialog
+        open={open}
+        handleClose={handleClose}
+        handleConfirm={handleConfirm}
+      />
+    </>
   );
 };
