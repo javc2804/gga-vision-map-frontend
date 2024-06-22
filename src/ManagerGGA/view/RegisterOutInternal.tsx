f14615f11df698358c91798cff15a5923a8ab467
javier <javc2804@gmail.com>
2024-06-21 21:45:15 -0400

    maqueta de listado de funcionamientos

--- src/ManagerGGA/view/RegisterOutInternal.tsx
+++ src/ManagerGGA/view/RegisterOutInternal.tsx
@@ -1,5 +1,19 @@
+import { TextField, Grid } from "@mui/material";
+
 const RegisterOutInternal = () => {
-  return <div>RegisterOutInternal</div>;
+  return (
+    <Grid container spacing={2} padding={2}>
+      {Array.from({ length: 31 }).map((_, index) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+          <TextField
+            fullWidth
+            label={`Campo ${index + 1}`}
+            variant="outlined"
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default RegisterOutInternal;
