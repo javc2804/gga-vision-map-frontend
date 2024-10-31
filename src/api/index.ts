76007d6c5561d083f6b1cc9c49d5092644f2f603
Javier <javc2804@gmail.com>
2024-10-31 00:49:22 +0000

    prueba

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,13 @@
+// import axios from "axios";
+
+// export const API_URL = axios.create({ baseURL: "http://localhost:3000" });
+// export const apiTerceros = axios.create({
+//   baseURL: "https://api-terceros.com",
+// });
+
 import axios from "axios";
 
-export const API_URL = axios.create({ baseURL: "http://localhost:3000" });
+export const API_URL = axios.create({ baseURL: "http://192.168.0.112:3000" });
 export const apiTerceros = axios.create({
   baseURL: "https://api-terceros.com",
 });
