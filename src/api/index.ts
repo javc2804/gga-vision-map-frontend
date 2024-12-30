e083a116675561fb96969b0362d0623b4f9961b8
transmiranda <javc2804@gmail.com>
2024-12-30 20:55:25 +0000

    varios cambios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const API_URL_BASE = process.env.REACT_APP_API_URL || "http://localhost:3000";
-const API_TERCEROS_URL_BASE =
-  process.env.REACT_APP_API_TERCEROS_URL || "https://api-terceros.com";
+const API_URL_BASE = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_TERCEROS_URL_BASE = import.meta.env.VITE_API_TERCEROS_URL || "https://api-terceros.com";
 
 export const API_URL = axios.create({ baseURL: API_URL_BASE });
 export const apiTerceros = axios.create({ baseURL: API_TERCEROS_URL_BASE });
